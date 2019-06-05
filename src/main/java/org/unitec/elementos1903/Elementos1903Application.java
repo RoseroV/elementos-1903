b1c4fd3d7baf1ba2accede55d90272ec12de197e
Servicios Especiales <Servicios Especiales@SE09>
2019-06-04 21:30:20 -0500

    Crud

--- src/main/java/org/unitec/elementos1903/Elementos1903Application.java
+++ src/main/java/org/unitec/elementos1903/Elementos1903Application.java
@@ -1,13 +1,37 @@
 package org.unitec.elementos1903;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Elementos1903Application {
+public class Elementos1903Application implements CommandLineRunner{
 
+    @Autowired
+    RepoCliente repoCliente;
+    
 	public static void main(String[] args) {
 		SpringApplication.run(Elementos1903Application.class, args);
 	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        
+     //repoCliente.save(new Cliente(1,"Beatriz","verobetty1@hotmail.com"));
+     //repoCliente.save(new Cliente(2,"Fernanda","mariferjim@hotmail.com"));
+     
+     //Buscar todos
+    /* for(Cliente c: repoCliente.findAll()){
+        System.out.println(c);
+     }*/
+     
+    //Buscar por Id
+    //System.out.println(repoCliente.findById(2));
+    
+    //Borrar por Id
+    //repoCliente.deleteById(1);
+    
+    }
 
 }
