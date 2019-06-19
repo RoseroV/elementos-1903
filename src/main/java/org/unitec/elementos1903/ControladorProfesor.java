e82540e64a3a2723cc71acc6ba3929371ca28804
Servicios Especiales <Servicios Especiales@SE09>
2019-06-18 20:52:47 -0500

    18/06/2019

--- src/main/java/org/unitec/elementos1903/ControladorProfesor.java
+++ src/main/java/org/unitec/elementos1903/ControladorProfesor.java
@@ -9,8 +9,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,8 +49,52 @@ public class ControladorProfesor {
         return e;
     }
     
+    //Buscar todos
     @GetMapping("/profesor")
     public List<Profesor> buscarTodos(){
         return repoProfe.findAll();
     }
+    
+    //Borrar
+    @DeleteMapping("/profesor/{id}")
+    public Estatus borrar (@PathVariable Integer id)
+    {
+        repoProfe.deleteById(id);
+        Estatus e=new Estatus();
+        e.setSuccess(true);
+        e.setMensaje("Profesor borrado con exito");
+        return e;
+    }
+    
+    //Actualizar
+    
+    @PutMapping("/profesor")
+        
+    public Estatus actualizar(@RequestBody String json)throws Exception{
+    
+        //Primero vamos a recibir el json del cliente web y lo transformamos a un objeto
+        //Java con la clase ObjectMapper
+        
+        ObjectMapper maper=new ObjectMapper();
+        //Ahora si lo leemos
+        
+        Profesor profe= maper.readValue(json, Profesor.class);
+        
+        repoProfe.save(profe);
+        
+        //Generamos el status
+        
+        Estatus e=new Estatus();
+        e.setMensaje("Profe Guardado con EXITO");
+        e.setSuccess(true);
+        
+        return e;
+    }
+    
+    //Buscar por ID
+    @GetMapping("/profesor/{id}")
+    public Profesor buscarPorId(@PathVariable Integer id){
+    return repoProfe.findById(id).get();
+    }
 }
+
