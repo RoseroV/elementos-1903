1739b877e9f331631a7afb6c174306258ab8cc75
Marifer <Marifer@DESKTOP-A1LUMN5>
2019-06-26 13:17:00 -0500

    Modificaciones

---
+++ src/main/java/org/unitec/elementos1903/RepoAlumno.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.unitec.elementos1903;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ *
+ * @author Marifer
+ */
+
+public interface RepoAlumno extends MongoRepository<Alumno, String>{
+    Alumno findByEmailAndCuenta(String email, String cuenta);
+}
+
